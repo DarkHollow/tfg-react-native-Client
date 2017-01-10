bd05ab2f153f25fe6f67e7db346aea8a82e002e0
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-01-10 05:18:25 +0100

    Importada la nuvea vista Serie -También aplicado efecto para el push y pop de esta nueva vista (fade bottom)

--- index.android.js
+++ index.android.js
@@ -11,6 +11,7 @@ import {
 
 import Root from './root';
 import Search from './search.android';
+import Series from './series';
 import SearchResults from './searchResults';
 
 export default class TrendingSeriesClient extends Component {
@@ -35,6 +36,9 @@ export default class TrendingSeriesClient extends Component {
     if (route.name == 'search') {
       return <Search navigator={navigator} />
     }
+    if (route.name == 'series') {
+      return <Series navigator={navigator} {...route.passProps} />
+    }
     if (route.name == 'searchResults') {
       return <SearchResults navigator={navigator} {...route.passProps} />
     }
@@ -50,6 +54,8 @@ export default class TrendingSeriesClient extends Component {
           configureScene={(route) => {
             if (route.name == 'search') {
               return Navigator.SceneConfigs.FadeAndroid;
+            } else if(route.name == 'series') {
+              return Navigator.SceneConfigs.FloatFromBottomAndroid;
             } else {
               return Navigator.SceneConfigs.FloatFromBottomAndroid;
             }
--- index.ios.js
+++ index.ios.js
@@ -10,6 +10,7 @@ import {
 
 import Root from './root';
 import Search from './search.ios';
+import Series from './series';
 import SearchResults from './searchResults';
 
 export default class TrendingSeriesClient extends Component {
@@ -22,6 +23,9 @@ export default class TrendingSeriesClient extends Component {
     }
     if (route.name == 'search') {
       return <Search navigator={navigator} />
+    }
+    if (route.name == 'series') {
+      return <Series navigator={navigator} {...route.passProps} />
     }
     if (route.name == 'searchResults') {
       return <SearchResults navigator={navigator} {...route.passProps} />
@@ -38,6 +42,8 @@ export default class TrendingSeriesClient extends Component {
           configureScene={(route) => {
             if (route.name == 'search') {
               return Navigator.SceneConfigs.PushFromRight;
+            } else if (route.name == 'series') {
+              return Navigator.SceneConfigs.FloatFromBottom;
             } else {
               return Navigator.SceneConfigs.PushFromRight;
             }
