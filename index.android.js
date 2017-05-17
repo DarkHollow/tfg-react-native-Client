84b9364e2d31da69f06e0bfcd95ae3fae782250c
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-05-17 15:16:15 +0200

    Gestión de JWT al inicio de la aplicación Comprobamos si existe el token y si es válido navegamos a la app Si no existe o es inválido navegamos a login Cambios de estilos y pantalla de activity indicator

--- index.android.js
+++ index.android.js
@@ -5,8 +5,12 @@ import {
   View,
   StatusBar,
   BackHandler,
+  AsyncStorage,
+  ActivityIndicator,
+  Platform,
 } from 'react-native';
 import CustomComponents from 'react-native-deprecated-custom-components';
+import LinearGradient from 'react-native-linear-gradient';
 
 import Root from './root';
 import Login from './login';
@@ -16,6 +20,68 @@ import TvShow from './tvshow';
 import RequestTvShow from "./requestTvShow";
 
 export default class TrendingSeriesClient extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      initialRoute: '',
+      loading: true,
+    };
+  }
+
+  processCheckJWTResponse(data) {
+    return !!data.ok;
+  }
+
+  setInitialRoute(route) {
+    this.setState({"initialRoute": route});
+    this.setState({"loading": false});
+  }
+
+  checkJWT() {
+    // comprobamos sesion (token jwt)
+    console.log("comprobando jwt almacenado");
+    return AsyncStorage.getItem("jwt")
+      .then((jwt) => {
+        console.log("token almacenado: " + jwt);
+        if (jwt !== null) {
+          // verificar token
+          console.log("Verificando token");
+          const URL = (Platform.OS === 'ios') ?
+            'http://localhost:9000/api/user/session' : 'http://192.168.1.13:9000/api/user/session';
+
+          fetch(URL, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + jwt,
+            },
+          }).then((response) => response.json())
+            .finally((responseData) => {
+              if (this.processCheckJWTResponse(responseData)) {
+                // token correcto, vamos a la app
+                this.setInitialRoute('root');
+              } else {
+                // token incorrecto, vamos a login
+                this.setInitialRoute('login');
+              }
+            }).catch((error) => {
+            // error fetching
+            console.error(error);
+            this.setInitialRoute('login');
+          });
+        } else {
+          // no hay token almacenado
+          console.log("No hay token almacenado");
+          this.setInitialRoute('login');
+        }
+      });
+  }
+
+  componentWillMount() {
+    this.checkJWT();
+  }
 
   renderScene(route, navigator) {
     console.log(route);
@@ -52,27 +118,38 @@ export default class TrendingSeriesClient extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar animated backgroundColor={'#2f3e9e'} />
-        <CustomComponents.Navigator
-          style={{backgroundColor: 'rgba(80,80,80,1)'}}
-          initialRoute={{ name: 'root'}}
-          renderScene={this.renderScene.bind(this)}
-          configureScene={(route) => {
-            if (route.name === 'register') {
-              return CustomComponents.Navigator.SceneConfigs.FloatFromRightAndroid;
-            } else if (route.name === 'search') {
-              return CustomComponents.Navigator.SceneConfigs.FadeAndroid;
-            } else if(route.name === 'tvshow') {
-              return CustomComponents.Navigator.SceneConfigs.FloatFromBottomAndroid;
-            } else {
-              return CustomComponents.Navigator.SceneConfigs.FloatFromBottomAndroid;
-            }
-          }}
-        />
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <LinearGradient style={[styles.container, {alignItems: 'center', justifyContent: 'center'}]}
+                        start={{x: 1, y: 0}} end={{x: 0.2, y: 1}}
+                        colors={['#1d1d1d', '#303030']}>
+          <ActivityIndicator style={styles.loader}
+                             size={'large'} color={'rgba(255,149,0,1)'} />
+        </LinearGradient>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar animated backgroundColor={'#2f3e9e'}/>
+          <CustomComponents.Navigator
+            style={{backgroundColor: 'rgba(80,80,80,1)'}}
+            initialRoute={{name: this.state.initialRoute}}
+            renderScene={this.renderScene.bind(this)}
+            configureScene={(route) => {
+              if (route.name === 'register') {
+                return CustomComponents.Navigator.SceneConfigs.FloatFromRightAndroid;
+              } else if (route.name === 'search') {
+                return CustomComponents.Navigator.SceneConfigs.FadeAndroid;
+              } else if (route.name === 'tvshow') {
+                return CustomComponents.Navigator.SceneConfigs.FloatFromBottomAndroid;
+              } else {
+                return CustomComponents.Navigator.SceneConfigs.FloatFromBottomAndroid;
+              }
+            }}
+          />
+        </View>
+      );
+    }
   }
 }
 
--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  AsyncStorage,
   StyleSheet,
   View,
+  ActivityIndicator,
   StatusBar,
+  Platform,
 } from 'react-native';
 import CustomComponents from 'react-native-deprecated-custom-components';
+import LinearGradient from 'react-native-linear-gradient';
 
 import Root from './root';
 import Login from './login';
@@ -15,6 +19,68 @@ import TvShow from './tvshow';
 import RequestTvShow from "./requestTvShow";
 
 export default class TrendingSeriesClient extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      initialRoute: '',
+      loading: true,
+    };
+  }
+
+  processCheckJWTResponse(data) {
+    return !!data.ok;
+  }
+
+  setInitialRoute(route) {
+    this.setState({"initialRoute": route});
+    this.setState({"loading": false});
+  }
+
+  checkJWT() {
+    // comprobamos sesion (token jwt)
+    console.log("comprobando jwt almacenado");
+    return AsyncStorage.getItem("jwt")
+      .then((jwt) => {
+        console.log("token almacenado: " + jwt);
+        if (jwt !== null) {
+          // verificar token
+          console.log("Verificando token");
+          const URL = (Platform.OS === 'ios') ?
+            'http://localhost:9000/api/user/session' : 'http://192.168.1.13:9000/api/user/session';
+
+          fetch(URL, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + jwt,
+            },
+          }).then((response) => response.json())
+          .finally((responseData) => {
+            if (this.processCheckJWTResponse(responseData)) {
+              // token correcto, vamos a la app
+              this.setInitialRoute('root');
+            } else {
+              // token incorrecto, vamos a login
+              this.setInitialRoute('login');
+            }
+          }).catch((error) => {
+            // error fetching
+            console.error(error);
+            this.setInitialRoute('login');
+          });
+        } else {
+          // no hay token almacenado
+          console.log("No hay token almacenado");
+          this.setInitialRoute('login');
+        }
+      });
+  }
+
+  componentWillMount() {
+    this.checkJWT();
+  }
 
   renderScene(route, navigator) {
     console.log(route);
@@ -40,34 +106,44 @@ export default class TrendingSeriesClient extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar animated />
-        <CustomComponents.Navigator
-          style={{backgroundColor: '#3e50b4'}}
-          initialRoute={{ name: 'root'}}
-          renderScene={this.renderScene.bind(this)}
-          configureScene={(route) => {
-            if (route.name === 'search') {
-              return CustomComponents.Navigator.SceneConfigs.PushFromRight;
-            } else if (route.name === 'tvshow') {
-              return CustomComponents.Navigator.SceneConfigs.FloatFromBottom;
-            } else if (route.name === 'requestTvShow') {
-              return CustomComponents.Navigator.SceneConfigs.FloatFromBottom;
-            } else {
-              return CustomComponents.Navigator.SceneConfigs.PushFromRight;
-            }
-          }}
-        />
-      </View>
-    );
+    if (this.state.loading) {
+      return (
+        <LinearGradient style={[styles.container, {alignItems: 'center', justifyContent: 'center'}]}
+                        start={{x: 1, y: 0}} end={{x: 0.2, y: 1}}
+                        colors={['#1d1d1d', '#303030']}>
+          <ActivityIndicator style={styles.loader}
+                             size={'large'} color={'rgba(255,149,0,1)'} />
+        </LinearGradient>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar animated/>
+          <CustomComponents.Navigator
+              style={{backgroundColor: '#3e50b4'}}
+              initialRoute={{ name: this.state.initialRoute}}
+              renderScene={this.renderScene.bind(this)}
+              configureScene={(route) => {
+                if (route.name === 'search') {
+                  return CustomComponents.Navigator.SceneConfigs.PushFromRight;
+                } else if (route.name === 'tvshow') {
+                  return CustomComponents.Navigator.SceneConfigs.FloatFromBottom;
+                } else if (route.name === 'requestTvShow') {
+                  return CustomComponents.Navigator.SceneConfigs.FloatFromBottom;
+                } else {
+                  return CustomComponents.Navigator.SceneConfigs.PushFromRight;
+                }
+              }}
+          />
+        </View>
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eeeeee',
   }
 });
 
