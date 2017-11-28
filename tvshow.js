2cc29174df2e41abf94d8e7372d98aeb3fabb680
Roberto Cánovas <rcanovas.corp@gmail.com>
2017-11-28 01:58:28 +0100

    Fix android bottom padding

--- tvshow.js
+++ tvshow.js
@@ -1163,7 +1163,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     paddingLeft: 20,
     paddingRight: 20,
-    paddingBottom: 20,
+    paddingBottom: 10,
     backgroundColor: '#212121',
   },
   footerTitleView: {
