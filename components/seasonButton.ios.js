6a535a2bb9d2a55acd3b2192a7503e9e3a473771
Roberto Cánovas <rcanovas.corp@gmail.com>
2018-01-28 09:58:54 +0100

    Mostrar contador episodios en botones de temporada

--- components/seasonButton.ios.js
+++ components/seasonButton.ios.js
@@ -5,6 +5,7 @@ import {
   View,
   TouchableOpacity,
   Image,
+  Platform,
 } from 'react-native';
 
 class seasonButton extends Component {
@@ -54,7 +55,13 @@ class seasonButton extends Component {
                 ]
               }
               source={this.props.source}
-            />
+            >
+              {this.props.unseenCount !== null ? this.props.unseenCount > 0 ?
+                (<View style={styles.posterUnseen}>
+                  <Text style={styles.posterUnseenText}>{this.props.unseenCount}</Text>
+                </View>)
+                : null : null}
+            </Image>
             <View style={styles.titleAndSubtitle}>
               <Text style={
                   [
@@ -102,6 +109,42 @@ const styles = StyleSheet.create({
   },
   subtitle: {
     fontWeight: '500',
+  },
+  posterUnseen: {
+    alignSelf: 'flex-end',
+    backgroundColor: 'rgba(255,149,0,0.76)',
+    paddingRight: 8,
+    paddingLeft: 8,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(0,0,0,1)',
+        shadowOffset: { width: 0, height: 0},
+        shadowOpacity: 0.6,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 3,
+      }
+    }),
+  },
+  posterUnseenText: {
+    color: 'rgba(255,255,255,1)',
+    textShadowOffset: { width: 1, height: 1},
+    textShadowColor: 'rgba(0,0,0,0.1)',
+    textShadowRadius: 3,
+    ...Platform.select({
+      ios: {
+        fontSize: 13,
+        lineHeight: 24,
+        fontWeight: '600',
+      },
+      android: {
+        fontSize: 13,
+        lineHeight: 20,
+        paddingBottom: 3,
+        fontWeight: 'Roboto-Medium',
+      }
+    }),
   },
 });
 
